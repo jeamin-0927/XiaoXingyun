770bb81334889d63da5c4392376501aae5dab3fc
최재민 <admin@chicken-moo.com>
2023-05-02 08:23:12 +0900

    Converting to Simplified Characters

--- src/lyrics.js
+++ src/lyrics.js
@@ -6,43 +6,43 @@ export default [
     endTime: "0:14"
   },
   {
-    chinese: "我聽見雨滴落在青青草地",
+    chinese: "我听见雨滴落在青青草地",
     IPA: "wǒ tīngjiàn yǔdī luò zài qīng qīng cǎodì",
     korean: "<1절> 난 푸른 잔디에 빗방울이 떨어지는 소리를 들었어",
     endTime: "00:20"
   },
   {
-    chinese: "我聽見遠方下課鐘聲響起",
+    chinese: "我听见远方下课钟声响起",
     IPA: "wǒ tīngjiàn yuǎnfāng xiàkè zhōngshēng xiǎngqǐ",
     korean: "나는 멀리서 수업이 마치는 종소리가 울려 퍼지는 것도 들었어",
     endTime: "00:26"
   },
   {
-    chinese: "可是我沒有聽見你的聲音",
+    chinese: "可是我没有听见你的声音",
     IPA: "Kěshì wǒ méiyǒu tīngjiàn nǐ de shēngyīn",
     korean: "하지만 나는 너의 목소리는 듣지 못했어",
     endTime: "00:31"
   },
   {
-    chinese: "認真 呼喚我姓名",
+    chinese: "认真呼唤我姓名",
     IPA: "Rènzhēn hūhuàn wǒ xìngmíng",
     korean: "열심히 나의 이름을 부르는",
     endTime: "00:37"
   },
   {
-    chinese: "愛上你的時候還不懂感情",
+    chinese: "爱上你的时候还不懂感情",
     IPA: "Ài shàng nǐ de shíhòu hái bù dǒng gǎnqíng",
     korean: "너를 사랑하게 되었을 때에도 아직 이해하지 못한 감정을",
     endTime: "00:44"
   },
   {
-    chinese: "離別了才覺得刻骨銘心",
+    chinese: "离别了才觉得刻骨铭心",
     IPA: "Líbié le cái juédé kègǔmíng xīn",
     korean: "이별하고 나서야 비로소 가슴 속 깊이 새기게 되었어",
     endTime: "00:50"
   },
   {
-    chinese: "為什麼沒有發現遇見了你",
+    chinese: "为什么没有发现遇见了你",
     IPA: "Wèishénme méiyǒu fāxiàn yùjiàn le nǐ",
     korean: "왜 알아차리지 못하였을까 너를 만난 것이",
     endTime: "00:55"
@@ -54,43 +54,43 @@ export default [
     endTime: "1:01"
   },
   {
-    chinese: "也許當時忙著微笑和哭泣",
+    chinese: "也许当时忙着微笑和哭泣",
     IPA: "Yěxǔ dāngshí máng zhe wéixiào hé kūqì",
-    korean: "<후렴> 아마도 그때는 웃고 웃느라 바빴겠지",
+    korean: "<후렴> 아마도 그때는 웃고 우느라 바빴겠지",
     endTime: "1:07"
   },
   {
-    chinese: "忙著追逐天空中的流星",
+    chinese: "忙着追逐天空中的流星",
     IPA: "Máng zhe zhuīzhú tiānkōng zhōng de liúxīng",
     korean: "하늘의 유성을 쫓아가느라 바빴나봐",
     endTime: "1:12"
   },
   {
-    chinese: "人理所當然的忘記",
+    chinese: "人理所当然的忘记",
     IPA: "Rén lǐsuǒdāngrán de wàngjì",
     korean: "사람은 당연하게 잊어버리나봐",
     endTime: "1:17"
   },
   {
-    chinese: "是誰風裏雨裏一直默默守護在原地",
+    chinese: "是谁风里雨里一直默默守护在原地",
     IPA: "Shì shéi fēng lǐ yǔ lǐ yìzhí mòmò shǒuhù zài yuándì",
     korean: "누가 비바람 속에서 줄곧 묵묵히 제자리에서 지켜주는지",
     endTime: "1:25"
   },
   {
-    chinese: "原來你是我最想留住的幸運",
+    chinese: "原来你是我最想留住的幸运",
     IPA: "Yuánlái nǐ shì wǒ zuì xiǎng liúzhù de xìngyùn",
     korean: "원래 너는 내가 가장 잡고 싶었던 행운이었어",
     endTime: "1:30"
   },
   {
-    chinese: "原來我們和愛情曾經靠得那麼近",
+    chinese: "原来我们和爱情曾经靠得那么近",
     IPA: "Yuánlái wǒmen hé àiqíng céngjīng kào dé nàme jìn",
     korean: "원래 우린 사랑이라는 감정과 일찍히 가까웠었지",
     endTime: "1:36"
   },
   {
-    chinese: "那為我對抗世界的決定",
+    chinese: "那为我对抗世界的决定",
     IPA: "Nà wèi wǒ duìkàng shìjiè de juédìng",
     korean: "날 위해 세상에 저항하려는 저 결심",
     endTime: "1:41"
@@ -102,31 +102,31 @@ export default [
     endTime: "1:47"
   },
   {
-    chinese: "一塵不染的真心 與你相遇 好幸運",
+    chinese: "一尘不染的真心 与你相遇好幸运",
     IPA: "Yìchénbùrǎn de zhēnxīn yǔ nǐ xiāngyù hǎo xìngyùn",
     korean: "티끌 하나 없이 순수한 너의 진심이었어 너와 만난 건 정말 행운이야",
     endTime: "1:54"
   },
   {
-    chinese: "可我也失去為你淚流滿面的權利",
+    chinese: "可我已失去为你泪流满面的权力",
     IPA: "Kě wǒ yǐ shīqù wèi nǐ lèiliú mǎnmiàn de quán lì",
     korean: "하지만 나는 이미 너를 위해 눈물을 흘릴 권력을 잃어버렸지",
     endTime: "2:00"
   },
   {
-    chinese: "但願在我看不到的天際",
+    chinese: "但愿在我看不到的天际",
     IPA: "Dàn yuàn zài wǒ kànbúdào de tiānjì",
     korean: "하지만 바라기를 내가 보이지 않는 하늘 끝에서",
     endTime: "2:05"
   },
   {
-    chinese: "你張開了雙翼 遇見你的註定",
+    chinese: "你张开了双翼 遇见你的注定",
     IPA: "Nǐ zhāngkāi le shuāngyì yùjiànnǐ de zhùdìng",
     korean: "너의 두 날개를 펴고 너의 운명을 만나길 바래",
     endTime: "2:12"
   },
   {
-    chinese: "她會有多幸運",
+    chinese: "她会有多幸运?",
     IPA: "Tā huì yǒu duō xìngyùn",
     korean: "그녀는 얼마나 행운일까?",
     endTime: "2:18"
@@ -144,61 +144,61 @@ export default [
     endTime: "2:33"
   },
   {
-    chinese: "擁有著後知後覺的美麗",
+    chinese: "拥有着后知后觉的美丽",
     IPA: "Yǒngyǒu zhe hòuzhīhòujué de měilì",
     korean: "지나간 후에 깨닫는 아름다움을 가지고 있지",
     endTime: "2:39"
   },
   {
-    chinese: "來不及感謝是你給我勇氣",
+    chinese: "来不及感谢是你给我勇气",
     IPA: "Láibují gǎnxiè shì nǐ gěi wǒ yǒngqì",
     korean: "고맙다고 하지도 못했네 나에게 용기를 준 너에게",
     endTime: "2:44"
   },
   {
-    chinese: "讓我能做回我自己",
+    chinese: "让我能做回我自己",
     IPA: "Ràng wǒ néng zuò huí wǒ zìjǐ",
     korean: "나로 하여금 나 자신으로 다시 돌아갈 수 있게 한",
     endTime: "2:49"
   },
   {
-    chinese: "也許當時忙著微笑和哭泣",
+    chinese: "也许当时忙着微笑和哭泣",
     IPA: "Yěxǔ dāngshí máng zhe wéixiào hé kūqì",
-    korean: "<후렴> 아마도 그때는 웃고 웃느라 바빴겠지",
+    korean: "<후렴> 아마도 그때는 웃고 우느라 바빴겠지",
     endTime: "2:56"
   },
   {
-    chinese: "忙著追逐天空中的流星",
+    chinese: "忙着追逐天空中的流星",
     IPA: "Máng zhe zhuīzhú tiānkōng zhōng de liúxīng",
     korean: "하늘의 유성을 쫓아가느라 바빴나봐",
     endTime: "3:02"
   },
   {
-    chinese: "人理所當然的忘記",
+    chinese: "人理所当然的忘记",
     IPA: "Rén lǐsuǒdāngrán de wàngjì",
     korean: "사람은 당연하게 잊어버리나봐",
     endTime: "3:06"
   },
   {
-    chinese: "是誰風裏雨裏一直默默守護在原地",
+    chinese: "是谁风里雨里一直默默守护在原地",
     IPA: "Shì shéi fēng lǐ yǔ lǐ yìzhí mòmò shǒuhù zài yuándì",
     korean: "누가 비바람 속에서 줄곧 묵묵히 제자리에서 지켜주는지",
     endTime: "3:14"
   },
   {
-    chinese: "原來你是我最想留住的幸運",
+    chinese: "原来你是我最想留住的幸运",
     IPA: "Yuánlái nǐ shì wǒ zuì xiǎng liúzhù de xìngyùn",
     korean: "원래 너는 내가 가장 잡고 싶었던 행운이었어",
     endTime: "3:19"
   },
   {
-    chinese: "原來我們和愛情曾經靠得那麼近",
+    chinese: "原来我们和爱情曾经靠得那么近",
     IPA: "Yuánlái wǒmen hé àiqíng céngjīng kào dé nàme jìn",
     korean: "원래 우린 사랑이라는 감정과 일찍히 가까웠었지",
     endTime: "3:25"
   },
   {
-    chinese: "那為我對抗世界的決定",
+    chinese: "那为我对抗世界的决定",
     IPA: "Nà wèi wǒ duìkàng shìjiè de juédìng",
     korean: "날 위해 세상에 저항하려는 저 결심",
     endTime: "3:30"
@@ -210,31 +210,31 @@ export default [
     endTime: "3:36"
   },
   {
-    chinese: "一塵不染的真心 與你相遇 好幸運",
+    chinese: "一尘不染的真心 与你相遇好幸运",
     IPA: "Yìchénbùrǎn de zhēnxīn yǔ nǐ xiāngyù hǎo xìngyùn",
     korean: "티끌 하나 없이 순수한 너의 진심이었어 너와 만난 건 정말 행운이야",
     endTime: "3:44"
   },
   {
-    chinese: "可我也失去為你淚流滿面的權利",
+    chinese: "可我已失去为你泪流满面的权力",
     IPA: "Kě wǒ yǐ shīqù wèi nǐ lèiliú mǎnmiàn de quán lì",
     korean: "하지만 나는 이미 너를 위해 눈물을 흘릴 권력을 잃어버렸지",
     endTime: "3:50"
   },
   {
-    chinese: "但願在我看不到的天際",
+    chinese: "但愿在我看不到的天际",
     IPA: "Dàn yuàn zài wǒ kànbúdào de tiānjì",
     korean: "하지만 바라기를 내가 보이지 않는 하늘 끝에서",
     endTime: "3:54"
   },
   {
-    chinese: "你張開了雙翼 遇見你的註定",
+    chinese: "你张开了双翼 遇见你的注定",
     IPA: "Nǐ zhāngkāi le shuāngyì yùjiànnǐ de zhùdìng",
     korean: "너의 두 날개를 펴고 너의 운명을 만나길 바래",
     endTime: "4:01"
   },
   {
-    chinese: "她會有多幸運",
+    chinese: "她会有多幸运?",
     IPA: "Tā huì yǒu duō xìngyùn",
     korean: "그녀는 얼마나 행운일까?",
     endTime: "4:11"
